22b15f4844b08d833ee76fc46d767ddcc01c9467
Thugkeedxxx <techthugkeed@gmail.com>
2025-08-02 22:51:07 +0200

    Create home.js

---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+  return (
+    <div className="text-center mt-24">
+      <h1 className="text-4xl font-bold mb-6">Welcome to THUGKEED TECH UNLIMITED PANEL</h1>
+      <Link to="/login" className="text-blue-600 hover:underline">Login / Signup</Link>
+    </div>
+  );
+    }
